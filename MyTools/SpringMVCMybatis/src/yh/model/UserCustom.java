3ebd345917af79befddf0570f4d0230ae1f1afab
hdyang12 <hdyang12@163.com>
2016-08-21 23:59:39 +0800

    c

--- MyTools/SpringMVCMybatis/src/yh/model/UserCustom.java
+++
@@ -1,7 +0,0 @@
-package yh.model;
-//User扩展类
-public class UserCustom extends User{
-
-	private static final long serialVersionUID = 1L;
-
-}
--- README.md
+++
@@ -1,5 +0,0 @@
-﻿# MyTools
------
-各种工具
-# SpringMVC
-一个最简单的，使用了Spring+SpringMVC框架的项目，仅实现了@RequestMapping的使用
